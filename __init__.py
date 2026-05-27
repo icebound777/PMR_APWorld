icebound777/PMR_APWorld

commit a57852d020aecc43b0fcd1c2140993aa343cbdd2

Upstream Version Update: v0.30.0, full change log here https://pm64randomizer.com/changelog

__init__.py
@@ -9,7 +9,6 @@ from .SettingsString import load_settings_from_site_string
 from worlds.AutoWorld import World, WebWorld
 from . import Locations, options
 from .data.chapter_logic import areas_by_chapter, get_chapter_excluded_location_names
-from .data.enum_types import BlockType
 from .modules.modify_entrances import get_bowser_rush_pairs, get_bowser_shortened_pairs
 from .modules.random_audio import get_randomized_audio
 from .modules.random_map_mirroring import get_mirrored_map_list
@@ -31,12 +30,12 @@ from .Rules import set_rules
 from .modules.random_partners import get_rnd_starting_partners
 from .options import (SeedGoal, PaperMarioOptions, ShuffleKootFavors, PartnerUpgradeShuffle, HiddenBlockMode,
                       ShuffleSuperMultiBlocks, GearShuffleMode, StartingMap, BowserCastleMode, ShuffleLetters,
-                      ItemTraps, MirrorMode, ShufflePartners, ShuffleKeys, ShuffleDungeonEntrances, BossShuffle)
+                      ItemTraps, MirrorMode, ShufflePartners, ShuffleKeys, ShuffleDungeonEntrances, BossShuffle,
+                      SpiritRequirements, ConsumableItemPool, StartingBoots)
 from .data.node import Node
 from .data.starting_maps import starting_maps
 from .Rom import generate_output, PaperMarioDeltaPatch
 from Fill import fill_restrictive, remaining_fill
-from .modules.random_blocks import get_block_placement
 import pkg_resources
 from .client import PaperMarioClient  # unused but required for generic client to hook onto
 logger = logging.getLogger("Paper Mario")
@@ -107,7 +106,6 @@ class PaperMarioWorld(World):
 
         # For generation
         self.placed_items = []
-        self.placed_blocks = {}
         self.entrance_list = []
 
         self.required_spirits = []
@@ -161,7 +159,7 @@ class PaperMarioWorld(World):
 
         # LCL is not compatible with several options
         # Rather than generate with drastically different settings, compile list of incompatible settings
-        if self.options.require_specific_spirits.value and self.options.limit_chapter_logic.value:
+        if self.options.spirit_requirements.value == SpiritRequirements.option_Specific_And_Limit_Chapter_Logic:
             lcl_warnings = ""
             if self.options.koot_favors.value != ShuffleKootFavors.option_Vanilla:
                 lcl_warnings += "\n'koot_favors' must be set to vanilla"
@@ -179,10 +177,6 @@ class PaperMarioWorld(World):
                                 "chapter logic set to true, but the following settings are incompatible with limiting "
                                 "chapter logic: ") + lcl_warnings
                 raise ValueError(lcl_warnings)
-        elif self.options.limit_chapter_logic.value:
-            raise ValueError(f"Paper Mario: {self.player} ({self.multiworld.player_name[self.player]}) has limit "
-                             "chapter logic set to true. Specific star spirits must also be set to true if you wish to "
-                             "limit chapter logic")
 
         # Make sure it doesn't try to shuffle Koot coins if rewards aren't shuffled
         if self.options.koot_favors.value == ShuffleKootFavors.option_Vanilla:
@@ -230,8 +224,7 @@ class PaperMarioWorld(World):
                     self.options.start_with_lakilester.value = True
 
         # limit chapter logic only applies when using the specific star spirits setting
-        if not self.options.require_specific_spirits.value:
-            self.options.limit_chapter_logic.value = False
+        if self.options.spirit_requirements.value == SpiritRequirements.option_Any:
             self.required_spirits = []
             self.excluded_spirits = []
         else:
@@ -245,7 +238,7 @@ class PaperMarioWorld(World):
 
             self.required_spirits = chosen_spirits
 
-            if self.options.limit_chapter_logic.value:
+            if self.options.spirit_requirements.value == SpiritRequirements.option_Specific_And_Limit_Chapter_Logic:
                 self.excluded_spirits = remaining_spirits
 
                 for chapter in remaining_spirits:
@@ -269,14 +262,6 @@ class PaperMarioWorld(World):
                 logger.info(f"Paper Mario: {self.player} ({self.multiworld.player_name[self.player]}) had less total "
                             f"power stars than the required amount. New total set to 15% more than the larger "
                             f"requirement, restricted to 120 or fewer.")
-
-        # determine what blocks are what, shuffling if needed and setting them up to be used as locations
-        if not self.placed_blocks:
-            self.placed_blocks = get_block_placement(self.options.super_multi_blocks.value ==
-                                                     ShuffleSuperMultiBlocks.option_true,
-                                                     self.options.partner_upgrades.value >=
-                                                     PartnerUpgradeShuffle.option_Super_Block_Locations,
-                                                     self.random)
 
         # shuffle bosses - NYI
         self.battles, self.boss_chapter_map = get_boss_battles(self.options.boss_shuffle.value, self.random)
@@ -404,7 +389,8 @@ class PaperMarioWorld(World):
                 self.itempool.append(self.create_item(self.get_filler_item_name()))
 
         # remove prefill items from item pool to be randomized
-        self.itempool, self.pre_fill_items, self.dungeon_restricted_items, self.dro_shop_puzzle_items = self.divide_itempools()
+        (self.itempool, self.pre_fill_items,
+         self.dungeon_restricted_items, self.dro_shop_puzzle_items) = self.divide_itempools()
 
         self.multiworld.itempool.extend(self.itempool)
         self.remove_from_start_inventory.extend(removed_items)
@@ -541,7 +527,8 @@ class PaperMarioWorld(World):
 
         # ensure DDO shop has 3 cheap consumables for puzzle purposes if needed
         if self.options.random_puzzles.value and self.options.include_shops.value and not (
-                self.options.limit_chapter_logic.value and 2 in self.excluded_spirits):
+                self.options.spirit_requirements.value == SpiritRequirements.option_Specific_And_Limit_Chapter_Logic and
+                2 in self.excluded_spirits):
             for item in self.itempool:
                 if (item_table[item.name][0] == "ITEM"
                         and item_table[item.name][3] <= 10 and item_table[item.name][2] <= 0xFF
@@ -607,7 +594,8 @@ class PaperMarioWorld(World):
         state.sweep_for_advancements(locations=self.get_locations())
 
         if self.options.random_puzzles.value and self.options.include_shops.value and not (
-                self.options.limit_chapter_logic.value and 2 in self.excluded_spirits):
+                self.options.spirit_requirements.value == SpiritRequirements.option_Specific_And_Limit_Chapter_Logic and
+                2 in self.excluded_spirits):
             dro_shop_locations = [self.multiworld.get_location(location, self.player)
                                   for location in self.random.sample([location for location in location_table.keys()
                                                                       if "DDO Outpost 1 Shop Item" in location], 3)]
@@ -643,11 +631,16 @@ class PaperMarioWorld(World):
                 fill_restrictive(self.multiworld, prefill_state(state), locations, gear_items,
                                  single_player_placement=True, lock=True, allow_excluded=False)
 
-        # Place partner upgrade items in super block turned yellow block locations
+        # Place partner upgrade items in super block locations
         if self.options.partner_upgrades.value == PartnerUpgradeShuffle.option_Super_Block_Locations:
             upgrade_items = list(filter(lambda item: pm_is_item_of_type(item, "PARTNERUPGRADE"), self.pre_fill_items))
-            yellow_block_locations = [name for (name, value) in self.placed_blocks.items() if value == BlockType.YELLOW]
-            locations = list(filter(lambda location: location.name in yellow_block_locations,
+            super_block_locations = location_groups["SuperBlock"]
+
+            # multi coin block locations are also candidates if they're shuffled
+            if self.options.super_multi_blocks.value >= ShuffleSuperMultiBlocks.option_Shuffle:
+                super_block_locations.extend(location_groups["MultiCoinBlock"])
+
+            locations = list(filter(lambda location: location.name in super_block_locations,
                                     self.multiworld.get_unfilled_locations(player=self.player)))
             if isinstance(locations, list):
                 for item in upgrade_items:
@@ -695,7 +688,7 @@ class PaperMarioWorld(World):
 
         # Anything remaining in pre fill items is a consumable that got selected randomly to be kept local
         # LCL can really skew the item pool, so fill up the excluded locations to prevent generation errors
-        if self.options.limit_chapter_logic.value:
+        if self.options.spirit_requirements.value == SpiritRequirements.option_Specific_And_Limit_Chapter_Logic:
             locations = list(filter(lambda location: location.progress_type == LocationProgressType.EXCLUDED,
                                     self.multiworld.get_unfilled_locations(player=self.player)))
             if len(locations) <= len(self.pre_fill_items):
@@ -795,7 +788,7 @@ class PaperMarioWorld(World):
             "start_with_sushie": self.options.start_with_sushie.value,
             "start_with_lakilester": self.options.start_with_lakilester.value,
             "enemy_difficulty": self.options.enemy_difficulty.value,
-            "require_specific_spirits": self.options.require_specific_spirits.value,
+            "spirit_requirements": self.options.spirit_requirements.value,
             "starting_boots": self.options.starting_boots.value,
             "starting_hammer": self.options.starting_hammer.value,
             "starting_map": self.options.starting_map.value,
@@ -820,7 +813,6 @@ class PaperMarioWorld(World):
             "hidden_block_mode": self.options.hidden_block_mode.value,
             "cook_without_frying_pan": self.options.cook_without_frying_pan.value,
             "merlow_rewards_pricing": self.options.merlow_rewards_pricing.value,
-            "placed_blocks": self.placed_blocks,
             "required_spirits": self.required_spirits
         }
 

ItemPool.py
@@ -12,7 +12,6 @@ from .options import *
 from .data.item_exclusion import exclude_due_to_settings, exclude_from_taycet_placement
 from .modules.modify_itempool import get_trapped_itempool, get_randomized_itempool
 from BaseClasses import ItemClassification as Ic, LocationProgressType
-from .data.enum_types import BlockType
 from .Locations import location_factory
 from .data.chapter_logic import get_chapter_excluded_item_names, get_chapter_excluded_location_names
 
@@ -57,7 +56,7 @@ def get_pool_core(world: "PaperMarioWorld"):
     ch_excluded_items = []
     placed_items_excluded = {}
 
-    if world.options.limit_chapter_logic.value:
+    if world.options.spirit_requirements.value == SpiritRequirements.option_Specific_And_Limit_Chapter_Logic:
         ch_excluded_locations = get_chapter_excluded_location_names(world.excluded_spirits,
                                                                     world.options.letter_rewards.value)
         ch_excluded_items = get_chapter_excluded_item_names(world.excluded_spirits)
@@ -109,7 +108,9 @@ def get_pool_core(world: "PaperMarioWorld"):
 
             if location.identifier in ["DRO_01/ShopItemB", "DRO_01/ShopItemD", "DRO_01/ShopItemE"]:
                 shuffle_item = (world.options.random_puzzles.value and world.options.include_shops.value
-                                and not (world.options.limit_chapter_logic.value and 2 in world.excluded_spirits))
+                                and not (world.options.spirit_requirements.value ==
+                                         SpiritRequirements.option_Specific_And_Limit_Chapter_Logic and
+                                         2 in world.excluded_spirits))
             else:
                 shuffle_item = world.options.include_shops.value
 
@@ -178,6 +179,13 @@ def get_pool_core(world: "PaperMarioWorld"):
             if not shuffle_item:
                 location.disabled = True
 
+        if location.name in location_groups["MultiCoinBlock"]:
+            shuffle_item = (world.options.super_multi_blocks.value > ShuffleSuperMultiBlocks.option_Off)
+
+        if location.name in location_groups["SuperBlock"]:
+            shuffle_item = (world.options.super_multi_blocks.value > ShuffleSuperMultiBlocks.option_Off or
+                            world.options.partner_upgrades.value > PartnerUpgradeShuffle.option_Vanilla)
+
         if location.name in location_groups["Gear"]:
             # hammer 1 bush is special in that it is made to not be empty even if starting with hammer
             if location.name == "GR Jr. Troopa's Playground In Hammer Bush":
@@ -188,13 +196,6 @@ def get_pool_core(world: "PaperMarioWorld"):
             if not shuffle_item:
                 location.disabled = True
 
-        if location.name in location_groups["RandomBlock"]:
-            shuffle_item = world.placed_blocks[location.name] == BlockType.YELLOW
-
-            if not shuffle_item:
-                location.disabled = True
-                location.show_in_spoiler = False
-
         if location.name == "SSS Star Sanctuary Gift of the Stars":
             shuffle_item = world.options.shuffle_star_beam.value
 
@@ -203,7 +204,8 @@ def get_pool_core(world: "PaperMarioWorld"):
                 location.show_in_spoiler = False
 
         if location.name in ch_excluded_locations and item in ch_excluded_items:
-            shuffle_item = not world.options.limit_chapter_logic.value
+            shuffle_item = not (world.options.spirit_requirements.value ==
+                                SpiritRequirements.option_Specific_And_Limit_Chapter_Logic)
 
         # add it to the proper pool, or place the item
         if shuffle_item:
@@ -303,10 +305,16 @@ def get_pool_core(world: "PaperMarioWorld"):
     if world.options.starting_boots.value == StartingBoots.option_Jumpless:
         pool_progression_items.append("Progressive Boots")
 
-    # add two of each partner upgrade item, taking care not to add unplaceable ones (goompa upgrades)
+    # add two of each partner upgrade item, remove the generic ones
     if world.options.partner_upgrades.value != PartnerUpgradeShuffle.option_Vanilla:
         for i in range(0, 2):
-            pool_other_items.extend(item_groups["PartnerUpgrade"])
+            for upgrade in item_groups["PartnerUpgrade"]:
+                if upgrade == "Partner Upgrade":
+                    for _ in range (0, 8):
+                        pool_other_items.remove(upgrade)
+                else:
+                    pool_other_items.append(upgrade)
+
 
     # adjust item pools based on settings
     items_to_remove_from_pools = get_items_to_exclude(world)
@@ -376,7 +384,7 @@ def get_pool_core(world: "PaperMarioWorld"):
 
     # before adding traps, fill up the out of logic locations with items that aren't progression
     # wouldn't want traps to not make it into the multiworld pool, would we?
-    if world.options.limit_chapter_logic.value:
+    if world.options.spirit_requirements.value == SpiritRequirements.option_Specific_And_Limit_Chapter_Logic:
         world.random.shuffle(ch_excluded_locations)
 
         # shuffle items but then sort to put useful items in the front so that filler items go to out of logic locations first

Rom.py
@@ -57,7 +57,6 @@ def write_patch(
     output_directory: str,
     world,
     placed_items: list,
-    placed_blocks: dict,
     entrance_list: list,
     enemy_stats: list,
     battle_formations: list,
@@ -86,7 +85,6 @@ def write_patch(
     table_data = rom_table.generate_pairs(
         options=world.options,
         placed_items=placed_items,
-        placed_blocks=placed_blocks,
         entrances=entrance_list,
         actor_attributes=enemy_stats,
         move_costs=move_costs,
@@ -247,7 +245,6 @@ def generate_output(world, output_dir: str) -> None:
     write_patch(output_directory=output_dir,
                 world=world,
                 placed_items=placed_items,
-                placed_blocks=world.placed_blocks,
                 entrance_list=world.entrance_list,
                 enemy_stats=enemy_stats,
                 battle_formations=battle_formations,

RomTable.py
@@ -2,9 +2,8 @@
 
 from .data.RomOptionList import rom_option_table, ap_to_rom_option_table
 from .data.palettes_meta import MENU_COLORS
-from .modules.random_blocks import get_block_key
 from .options import (EnemyDamage, PaperMarioOptions, PartnerUpgradeShuffle, ShuffleKootFavors, ShuffleLetters,
-                      BowserCastleMode, StatusMenuColorPalette, EnemyDifficulty)
+                      BowserCastleMode, StatusMenuColorPalette, EnemyDifficulty, ShuffleSuperMultiBlocks)
 from .data.MysteryOptions import MysteryOptions
 from .data.starting_maps import starting_maps
 from .data.node import Node
@@ -27,7 +26,7 @@ class RomTable:
     def __getitem__(self, key):
         return self.db[key]
 
-    def generate_pairs(self, options: PaperMarioOptions, placed_items: list[Node], placed_blocks: dict, entrances: list,
+    def generate_pairs(self, options: PaperMarioOptions, placed_items: list[Node], entrances: list,
                        actor_attributes: list, move_costs: list, palettes: list, quizzes: list, music_list: list,
                        mapmirror_list: list, puzzle_list: list, mystery_opts: MysteryOptions, required_spirits: list,
                        battle_list: list, star_beam_area: int):
@@ -86,12 +85,6 @@ class RomTable:
                     "key": node.get_price_key(),
                     "value": node.current_item.base_price
                 })
-
-        for name, value in placed_blocks.items():
-            table_data.append({
-                "key": get_block_key(name),
-                "value": value
-            })
 
         # Entrances
         for key, value in entrances:
@@ -216,8 +209,7 @@ def get_dbtuples(options: PaperMarioOptions, mystery_opts: MysteryOptions, requi
         map_tracker_bits += 0x2000
     if options.bowser_castle_mode.value <= BowserCastleMode.option_Shortened:
         map_tracker_bits += 0x4000
-    if (options.partner_upgrades.value >= PartnerUpgradeShuffle.option_Super_Block_Locations
-            and options.super_multi_blocks.value):
+    if options.super_multi_blocks.value == ShuffleSuperMultiBlocks.option_Anywhere:
         map_tracker_bits += 0x8000
 
     map_tracker_check_bits = map_tracker_bits

SettingsString.py
@@ -56,12 +56,14 @@ difficultyMap = [
     SettingModel("p", "merlow_rewards_pricing", "number"),
     SettingModel("b", "badge_synergy", "bool"),
     SettingModel("v", "drop_star_points", "bool"),
+    SettingModel("o", "chet_rippo", "bool"),
+    SettingModel("!", "bowser_door_quiz", "number"),
+    SettingModel("@", "kent_c_koopa", "number"),
 ]
 
 goalsMap = [
     SettingModel("w", "star_way_spirits", "number"),
-    SettingModel("e", "require_specific_spirits", "bool"),
-    SettingModel("f", "limit_chapter_logic", "bool"),
+    SettingModel("r", "spirit_requirements", "number"),
     SettingModel("@", "star_beam_spirits", "number"),
     SettingModel("#", "star_beam_power_stars", "number"),
     SettingModel("s", "shuffle_star_beam", "bool"),
@@ -106,7 +108,7 @@ itemsMap = [
     SettingModel("i", "shuffleItems", "bool"),
     SettingModel("l", "letter_rewards", "number"),
     SettingModel("r", "trading_events", "bool"),
-    SettingModel("b", "super_multi_blocks", "bool"),
+    SettingModel("!", "super_multi_blocks", "number"),
     SettingModel("g", "gear_shuffle_mode", "number"),
     SettingModel("u", "partner_upgrades", "number"),
     SettingModel("a", "cheato_items", "number"),
@@ -283,7 +285,11 @@ def decompress_form_group(settings_string: str, cur_map: list, world):
                                     option = world.random.randint(0, 7)
                                 world.options.__dict__[cur_model.key].value = option
                             case _:
-                                world.options.__dict__[cur_model.key].value = int(value)
+                                try:
+                                    world.options.__dict__[cur_model.key].value = int(value)
+                                except ValueError:
+                                    raise ValueError(f'Invalid Paper Mario Settings String: invalid value for '
+                                                     f'{cur_model.key}.')
                     elif cur_model.key.startswith("start_partners_"):
                         if start_partners_min == -1:
                             start_partners_min = int(value)

commit 51e4b64a4414e42f5d7d0f40fd2e0483eed1e1df

fix: star way never expecting specific spirits
The variable `require_specific_spirits` is never defined, so I guess it
is always assumed to be False
Regression in a57852d0

__init__.py
@@ -219,9 +219,12 @@ class PaperMarioWorld(World):
 
         # limit chapter logic only applies when using the specific star spirits setting
         if self.options.spirit_requirements.value == SpiritRequirements.option_Any:
+            self.require_specific_spirits = False
             self.required_spirits = []
             self.excluded_spirits = []
         else:
+            self.require_specific_spirits = True
+
             # determine which star spirits are needed
             remaining_spirits = [i for i in range(1, 8)]
             chosen_spirits = []